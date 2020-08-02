862ff33001044b87606894c736f5931547db9a91
carninojo15 <carlosmasson96@gmail.com>
2020-08-02 17:54:33 -0400

    weights tested

--- src/Graph.java
+++ src/Graph.java
@@ -1,54 +1,29 @@
 
 public class Graph {
 	
+	class Edge{
+		int source, destination , weight;
+		
+		Edge(){
+			source = destination = weight = 0;
+		}
+	};
 	
-	class DirectExchange {
-		int source, destination, weight;
-		DirectExchange(){
-			this.source = 0;
-			this.destination = 0;
-			this. weight = 0;
+	int V, E;
+	Edge edge[];
+	
+	Graph(int vertices, int edges){
+		V = vertices;
+		E = edges;
+		edge = new Edge[edges];
+		
+		for(int i =0; i < edges; i++) {
+			edge[i]= new Edge();
 		}
 	}
 	
-	int currencies, exchanges;
-	DirectExchange directExchanges[];
+
 	
-	Graph(int currencies, int exchanges){
-		this.currencies = currencies;
-		this.exchanges = exchanges;
-		directExchanges = new DirectExchange[exchanges];
-		
-		for(int i =0; i < exchanges; i++ ) {
-			directExchanges[i] = new DirectExchange();
-		}
-	}
-	
-	
-	void BellmanFord(Graph graph, int source) {
-		int vector = graph.currencies;
-		int edge = graph.exchanges;
-		int distance[] = new int[vector];
-		
-		for(int i = 0; i < vector; i++) {
-			distance[i] = Integer.MAX_VALUE;
-		}
-		
-		distance[source] = 0;
-		
-		for(int i = 1; i < vector; i++) {
-			for(int j = 0; j < edge; j++){
-				int u = graph.directExchanges[j].source;
-				int v = graph.directExchanges[j].destination;
-				int weight = graph.directExchanges[j].weight;
-				
-				if(distance[u] != Integer.MAX_VALUE && distance[u] + weight < distance[v]) {
-					distance[v] = distance[u] + weight;
-				}
-			}
-		}
-	}
-	
-	
+
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,15 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Scanner br = new Scanner(new File("Book1.csv"));
+		Scanner br = new Scanner(new File("Book2.csv"));
 		
-		List<String[]> rowList = new ArrayList<String[]>();
+		List<String[]> rawData = new ArrayList<String[]>();
 		
 		
 		while(br.hasNext()) {
 			String line = br.nextLine();
 			String[] columns = line.split(","); 
-			rowList.add(columns);
+			rawData.add(columns);
 			
 			
 		}
@@ -27,36 +27,115 @@ public class Main {
 		
 		ArrayList<String> currencyNames = new ArrayList<>();
 		
-		for(int i = 1; i < rowList.get(0).length; i++) {
-			currencyNames.add(rowList.get(0)[i]);
+		for(int i = 1; i < rawData.get(0).length; i++) {
+			currencyNames.add(rawData.get(0)[i]);
 		}
 		
-		String rates [][] = new String [rowList.get(1).length-1][rowList.get(1).length-1];
+		float rates [][] = new float [rawData.get(1).length-1][rawData.get(1).length-1];
 		
-		for(int row = 1; row < rowList.size(); row++) {
-			for(int column = 1; column < rowList.get(1).length; column++) {
-				rates[row-1][column-1] = rowList.get(row)[column];
-				//System.out.println(rates[row-1][column-1]);
+		for(int row = 1; row < rawData.size(); row++) {
+			for(int column = 1; column < rawData.get(1).length; column++) {
+				float rate = Float.parseFloat(rawData.get(row)[column]);
+				rates[row-1][column-1] = rate ;
+				
+			}
+		}
+		
+		float weight[][] = new float[rates[0].length][rates[0].length];
+		
+		for(int row = 0; row < rates[0].length; row++) {
+			for(int column = 0; column < rates[0].length; column++) {
+				weight[row][column] = 1;
+				weight[row][column] -= Math.log10(rates[row][column]);
+				
 			}
 		}
 		
 		
 		
-		System.out.println(Arrays.deepToString(rates).replace("],","\n").replace(",", "\t| ").replaceAll("[\\[\\]]"," "));
 		
 		
+		//System.out.println( rates[2][3] + " 1 - log(5) = 0.3010299 : " + weight[2][3]);
+		//System.out.println( rates[0][3] + " 1 - log(12) = -07918124 : " + weight[0][3]);
 		
-//		Scanner sc= new Scanner(System.in);     
-//		System.out.print("Choose currency: ");  
-//		String a= sc.nextLine();  
-//		
-//		while(!currencyNames.contains(a)) {
-//			System.out.println("Invalid answer, try another \nEx: Kuwaiti Dinar, Japanese Yen ");
-//			a= sc.nextLine();
-//		}
-				
+		/* Scanner sc= new Scanner(System.in);  */   
+		
+		bellmanFord(currencyNames,rates,weight, 1);
+		
+		
 	}
 	
+	static void bellmanFord(ArrayList<String> currencies, float rates[][],float [][]weights, int source){
+		float dist[] = new float [currencies.size()+1];
+		
+		for(int i = 0; i < currencies.size(); i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
+		
+		dist[source] = 0; 
+		int u = 0 , v = 0;
+		for(int i = 0 , k =1; i < currencies.size() && k < currencies.size(); i++, k++){
+			u++;
+			dist[u] = rates[i][0];
+			
+			System.out.print("Source: " + dist[u] + "\tDestination: ");
+			for(int j = 0 ; j < currencies.size(); j++ ){
+				
+				dist[v]= rates[j][k];
+				System.out.print( dist[v] + " | ");
+				
+			}
+			System.out.print("\n");
+		}
+		
+		
+	}
+	
+	
+	
+///////////////////THIS IS TRASH/////////////////////////////////////////////	
+//	for(int i =0; i < currencyNames.size(); i++) {
+//		
+//		System.out.println((i+1) + " : " + currencyNames.get(i));
+//	}
+//	
+//	System.out.print("\nSelect a number based on currency of choice: ");
+//	
+//	int choice = sc.nextInt();
+//	
+//	System.out.println("\nYour choice was " + currencyNames.get(choice-1) + "\n");
+//	
+//	for(int i = 0; i < rates[0].length; i++) {
+//		System.out.println(currencyNames.get(i)+ " max Exchange Rate is: "+ rates[2][i] +", and direct rate is: " + rates[3][i]);
+////	}
+////	
+////	System.out.println("\nYour choice was " + currencyNames.get(choice-1));
+//	int V = houses.size();
+//	int E = V * V;
+//	float dist[] = new float[V];
+//	
+//	for(int i =0; i < V; i++) {
+//		dist[i] = Integer.MAX_VALUE; // initalize vertices as infinite
+//	}
+//	
+//	dist[source] = 0; // starting point
+//	
+//	for(int i = 0; i < V-1; i++) {
+//		//Update the edge
+//		float u = distances[0][i];
+//		
+//		for(int j = 0; j < E; j++) {
+//			float v = distances[i+1][j];
+//			float weight = weights[i+1][j];
+//			if((v != Integer.MAX_VALUE || u != 0 || v != 0 ) && u + weight < v) {
+//				u = v;
+//			}
+//		}
+//	}
+//	
 }
+   
+	
+
 
 
